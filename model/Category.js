8795a6c67a298789d071edfe1b6ee8108b11ea77
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-25 14:48:53 +0100

    created category for admin

--- model/Category.js
+++ model/Category.js
@@ -5,15 +5,15 @@ const categorySchema = new mongoose.Schema(
         user:{
             type:mongoose.Schema.Types.ObjectId,
             ref:"User",
-            required:true,
+            require:true,
         },
         title:{
-            title:"",
+            type:String,
             required:true,
         },
     },
     {
-        timestamps:true
+        timestamps:true,
     }
 );
 const Category = mongoose.model("Category",categorySchema);
