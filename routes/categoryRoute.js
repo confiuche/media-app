db7b8768ee95a4620bbed34a36be3f30051fadc4
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-25 15:10:50 +0100

    created a route for display all category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,12 +2,14 @@ import express from 'express'
 import { isLogin } from '../middlewares/isLogin.js';
 import { validateUser } from '../middlewares/userValidation.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
-import { createCategory } from '../controller/categoryController.js';
+import { createCategory, list } from '../controller/categoryController.js';
 
 
 const categoryRoutes = express.Router();
 
 //create category
 categoryRoutes.post("",isLogin,isAdmin,createCategory)
+//display list
+categoryRoutes.get("",list)
 
 export default categoryRoutes;
