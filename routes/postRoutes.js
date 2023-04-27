f65df672475a500af3c8322654bba61efd5e06c5
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-27 15:12:23 +0100

    created route for admin fetch post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createPostController } from '../controller/postController.js';
+import { createPostController, fetchAllPostByAdmin } from '../controller/postController.js';
 import { isLogin } from "../middlewares/isLogin.js"
+import { isAdmin } from '../middlewares/isAdmin.js'
 
 const postRoutes = express.Router();
 
+//Create Post
 postRoutes.post("/create",isLogin, createPostController)
+//list all post by admin
+postRoutes.get("",isLogin,isAdmin,fetchAllPostByAdmin)
 
 export default postRoutes;
