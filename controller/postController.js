5a3f0bc850529530bd3f99c05493ab2f25f64d22
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-27 10:05:46 +0100

    created logic for post controller

--- controller/postController.js
+++ controller/postController.js
@@ -1,11 +1,34 @@
-//create users
+import Post from "../model/PostModel.js";
+import User from "../model/userModel.js"
+
+//create post
 export const createPostController = async(req,res)=>{
-    const {title,description}=req.body;
+    const {title,description,category}=req.body;
     try{
+        //obtain the user who is creating the post
+        const postOwner = await User.findById(req.userAuth)
+        if(postOwner.isBlocked){
+            return res.json({
+                status:"error",
+                message:"Sorry access denied, because your account is currently blocked"
+            })
+        }
+        const createPost = await Post.create({
+            title,
+            description,
+            category,
+            user:postOwner._id
+        })
+
         res.json({
             status:"success",
-            data:`congratulation post successfully`
+            data:createPost
     })
+
+//attached the post to the owner
+postOwner.posts.push(createPost);
+await postOwner.save()
+
     } catch(error){
         res.json(error.message);
     }   
