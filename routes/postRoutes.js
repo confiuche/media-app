dd6fe161a5bdcfcb916fb6a6c2338e84f6073b8f
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-27 10:07:47 +0100

    created post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createPostController } from '../controller/postController.js';
+import { isLogin } from "../middlewares/isLogin.js"
 
 const postRoutes = express.Router();
 
-postRoutes.post("/create", createPostController)
+postRoutes.post("/create",isLogin, createPostController)
 
 export default postRoutes;
