b2971e6c1e49331eda86264aa837ccdc8c02e6d1
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-27 10:08:16 +0100

    logic category controller

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { isLogin } from '../middlewares/isLogin.js';
 import { validateUser } from '../middlewares/userValidation.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
-import { createCategory, displaySingleCategory, list } from '../controller/categoryController.js';
+import { createCategory, deleteCategory, displaySingleCategory, list, updateCategory } from '../controller/categoryController.js';
 
 
 const categoryRoutes = express.Router();
@@ -13,5 +13,9 @@ categoryRoutes.post("",isLogin,isAdmin,createCategory)
 categoryRoutes.get("",list)
 //get single category
 categoryRoutes.get("/:id",isLogin,displaySingleCategory)
+//delete category
+categoryRoutes.delete("/:id",isLogin,isAdmin,deleteCategory)
+//update category
+categoryRoutes.put("/:id",isLogin,updateCategory)
 
 export default categoryRoutes;
