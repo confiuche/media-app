f0d235399f34447cdf4866631958186813903041
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-28 06:51:23 +0100

    created route for user list all post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPostController, fetchAllPostByAdmin } from '../controller/postController.js';
+import { createPostController, fetchAllPostByAdmin, list } from '../controller/postController.js';
 import { isLogin } from "../middlewares/isLogin.js"
 import { isAdmin } from '../middlewares/isAdmin.js'
 
@@ -7,7 +7,10 @@ const postRoutes = express.Router();
 
 //Create Post
 postRoutes.post("/create",isLogin, createPostController)
-//list all post by admin
-postRoutes.get("",isLogin,isAdmin,fetchAllPostByAdmin)
+//list all post for admin
+postRoutes.get("/list-post-admin",isLogin,isAdmin,fetchAllPostByAdmin)
+//list all post
+postRoutes.get("/list-post",isLogin,list)
+
 
 export default postRoutes;
