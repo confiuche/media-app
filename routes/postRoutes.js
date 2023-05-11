1b23a2f22a5591357a3c608893ecf8fa6c97c249
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-05-11 07:30:34 +0100

    added upload photo to our route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { createPostController, fetchAllPostByAdmin, fetchPostByUser, getSinglePost, list } from '../controller/postController.js';
+import { createPostController, deletPost, deletPostByAdmin, fetchAllPostByAdmin, fetchPostByUser, getSinglePost, list } from '../controller/postController.js';
 import { isLogin } from "../middlewares/isLogin.js"
 import { isAdmin } from '../middlewares/isAdmin.js'
+import multer from 'multer';
+import storage from '../config/cloudinary.js'
 
+const upload = multer({storage});
 const postRoutes = express.Router();
 
 //Create Post
-postRoutes.post("/create",isLogin, createPostController)
+postRoutes.post("/create",isLogin,upload.single("image"), createPostController)
 //list all post for admin
 postRoutes.get("/list-post-admin",isLogin,isAdmin,fetchAllPostByAdmin)
 //list all post
@@ -15,6 +18,10 @@ postRoutes.get("/list-post",isLogin,list)
 postRoutes.get("/user/:id",isLogin,fetchPostByUser)
 //get single post
 postRoutes.get("/:id",isLogin,getSinglePost);
+//delete post by user
+postRoutes.delete("/:id",isLogin,deletPost);
+//delete post
+postRoutes.delete("/admin/:id",isLogin,isAdmin,deletPostByAdmin);
 
 
 export default postRoutes;
