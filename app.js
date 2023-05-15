8f6808ba20c4ce31e26485e15a1fb633f2993468
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-05-15 14:16:38 +0100

    app route created for comment

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import { database } from "./config/dBconnect.js";
 import categoryRoutes from "./routes/categoryRoute.js";
+import commentRoutes from "./routes/commentRoute.js";
+
 
 dotenv.config();
 database();
@@ -17,6 +19,7 @@ const PORT = process.env.PORT || 8080;
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/comment",commentRoutes);
 
 
 app.listen(PORT, console.log(`App started at ${PORT}`));
