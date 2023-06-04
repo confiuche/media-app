41b5e681955cf7d48c1f98db3ad674341c19ae78
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-04 16:29:07 +0100

    error handling created

---
+++ utils/AppErr.js
@@ -0,0 +1,8 @@
+const AppError = (message, statusCode) => {
+    let error = new Error(message);
+    error.statusCode = statusCode ? statusCode : 500;
+    error.stack = error.stack;
+    return error;
+};
+
+export default AppError
