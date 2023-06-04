fb0165cb22b4c0e4cd649f6847750b4a0f123a6d
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-04 16:27:40 +0100

    updated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import postRoutes from "./routes/postRoutes.js";
 import { database } from "./config/dBconnect.js";
 import categoryRoutes from "./routes/categoryRoute.js";
 import commentRoutes from "./routes/commentRoute.js";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+
 
 
 dotenv.config();
@@ -21,5 +23,6 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/comment",commentRoutes);
 
+app.use(globalErrorHandler);
 
 app.listen(PORT, console.log(`App started at ${PORT}`));
