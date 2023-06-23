d4e9a96be782fd9ac8b5072c3a6f3cac7a03a89f
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-23 20:12:33 +0100

    stripe added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { database } from "./config/dBconnect.js";
 import categoryRoutes from "./routes/categoryRoute.js";
 import commentRoutes from "./routes/commentRoute.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import { Stripe } from "stripe"
+import User from "./model/userModel.js"
 
 
 
@@ -13,6 +15,7 @@ dotenv.config();
 database();
 
 const app = express();
+
 
 app.use(express.json());
 
@@ -23,6 +26,17 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/comment",commentRoutes);
 
+app.get("/success", async (req, res) => {
+    res.send("<h1>Thanks your payment has been received</h1>");
+  });
+
 app.use(globalErrorHandler);
 
 app.listen(PORT, console.log(`App started at ${PORT}`));
+
+//for wrong route
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     message: `${req.originalUrl} not found`,
+//   });
+// });
