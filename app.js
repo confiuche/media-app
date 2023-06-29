9fb987f4701080e7aa414f77d71fcdba239e0604
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-29 06:49:36 +0100

    webhooks for stripe implemented

--- app.js
+++ app.js
@@ -17,6 +17,99 @@ database();
 const app = express();
 
 
+//webhook
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_18835a2568dea2d60674f842df18a8a7eeeae81b8381f6e2f9c538195139a4db";
+
+app.post('/webhook', express.raw({type: 'application/json'}), async (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    //console.log("event");
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  if(event.type === "checkout.session.completed"){
+    const session = event.data.object;
+    console.log(session);
+
+    const { userId } = session.metadata;
+    const totalAmount = session.amount_total;
+    const payment_status = session.payment_status;
+    const paymentMethod = session.payment_method_types[0];
+    const currency = session.currency;
+    const subscriptionDate = new Date(session.created * 1000);
+    const transationId = session.object.id;
+
+    // console.log({
+    //   userId,
+    //   totalAmount,
+    //   payment_status,
+    //   paymentMethod,
+    //   currency,
+    //   datePaid,
+    // });
+
+    const subscribeUser = await User.findByIdAndUpdate(
+      JSON.parse(userId),
+      {
+        subscription: {
+          totalAmount: totalAmount / 100,
+          payment_status,
+          paymentMethod,
+          currency,
+          transationId,
+          subscriptionDate,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    console.log(subscribeUser);
+  }
+
+  // switch (event.type) {
+  //   case 'payment_intent.succeeded':
+  //     const paymentIntentSucceeded = event.data.object;
+  //     // Then define and call a function to handle the event payment_intent.succeeded
+  //     break;
+  //   // ... handle other event types
+  //   default:
+  //     console.log(`Unhandled event type ${event.type}`);
+  // }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+}
+);
+
+
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
